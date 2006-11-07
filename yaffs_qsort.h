07193e461437b7478ac76f341328c8e5038da94f
charles <charles>
2006-11-07 23:20:09 +0000

    Replace with own version of header file

--- yaffs_qsort.h
+++ yaffs_qsort.h
@@ -1,41 +1,23 @@
 /*
- * Copyright (c) 2000-2002 Silicon Graphics, Inc.  All Rights Reserved.
+ * YAFFS: Yet another FFS. A NAND-flash specific file system.
+ * yaffs_qsort.h: Interface to BSD-licensed qsort routine.
  *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of version 2 of the GNU General Public License as
+ * Copyright (C) 2002 Aleph One Ltd.
+ *   for Toby Churchill Ltd and Brightstar Engineering
+ *
+ * Created by Charles Manning <charles@aleph1.co.uk>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 2.1 as
  * published by the Free Software Foundation.
  *
- * This program is distributed in the hope that it would be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *
- * Further, this software is distributed without any warranty that it is
- * free of the rightful claim of any third person regarding infringement
- * or the like.  Any license provided herein, whether implied or
- * otherwise, applies only to this software file.  Patent licenses, if
- * any, provided herein do not apply to combinations of this program with
- * other software, or any other product whatsoever.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write the Free Software Foundation, Inc., 59
- * Temple Place - Suite 330, Boston MA 02111-1307, USA.
- *
- * Contact information: Silicon Graphics, Inc., 1600 Amphitheatre Pkwy,
- * Mountain View, CA  94043, or:
- *
- * http://www.sgi.com
- *
- * For further information regarding this notice, see:
- *
- * http://oss.sgi.com/projects/GenInfo/SGIGPLNoticeExplan/
+ * $Id: yaffs_qsort.h,v 1.2 2006-11-07 23:20:09 charles Exp $
  */
 
-#ifndef QSORT_H
-#define QSORT_H
+#ifndef __YAFFS_QSORT_H__
+#define __YAFFS_QSORT_H__
 
-extern void qsort (void *const pbase,
-		    size_t total_elems,
-		    size_t size,
-		    int (*cmp)(const void *, const void *));
+extern void qsort (void *const base, size_t total_elems, size_t size,
+                   int (*cmp)(const void *, const void *));
 
 #endif

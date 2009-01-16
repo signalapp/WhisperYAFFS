021066ab1ea323a40804c87d82246e03e029eca9
charles <charles>
2009-01-16 00:49:25 +0000

    Replace test scrips with nand and nor versions

---
+++ direct/tests/init_fw_update_test_nand.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Run this to initialise the file system for the test runs.
+   ./yaffs_test  -u -i yaffs2
---
+++ direct/tests/run_fw_update_test_nand.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+for ((i=0; i < 100000; i++))  
+do
+
+   seed=$RANDOM   
+   j=$(( $i % 10 ))
+   rm seed*$j
+   echo $seed>seed-for-run-$i
+   rm emfile-2k-0-*$j
+   cp emfile-2k-0 emfile-2k-0-$i
+   echo "######### Run $i with seed $seed"
+   ./yaffs_test -u -p -s$seed yaffs2
+done

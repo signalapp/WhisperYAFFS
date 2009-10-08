5fdeee70197161d59e3022372d424594c291ce5e
charles <charles>
2009-10-08 02:16:17 +0000

    More python stuff

--- direct/python/yaffs.i
+++ direct/python/yaffs.i
@@ -1,6 +1,8 @@
 %module yaffs
 %{
 #include "yaffsfs_for_swig.h"
+void yaffs_print_constants(void);
 %}
 
 %include "yaffsfs_for_swig.h"
+void yaffs_print_constants(void);
--- direct/python/yaffs_swig_helper.c
+++ direct/python/yaffs_swig_helper.c
@@ -1,4 +1,21 @@
+/*
+ * These are some dangly bits that need to be built to wrap up the rest of the yaffs test harness
+ * in SWIG.
+ *
+ * This is also the place where extra debugging stuff might end up.
+ *
+ */
+ 
 #include "yaffsfs.h"
 
 int simulate_power_failure;
 int random_seed;
+
+int yaffs_print_constants(void)
+{
+	printf( "O_CREAT........%d\n",O_CREAT);
+	printf( "O_RDONLY.......%d\n",O_RDONLY);
+	printf( "O_WRONLY.......%d\n",O_WRONLY);
+	printf( "O_RDWR.........%d\n",O_RDWR);
+	printf( "O_TRUNC........%d\n",O_TRUNC);
+}

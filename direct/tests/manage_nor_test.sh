99c62e39578113a798483b5dc6c13a80737163f1
Charles Manning <cdhmanning@gmail.com>
2010-03-25 13:48:41 +1300

    Add more test scripts to automate running tests.

---
+++ direct/tests/manage_nor_test.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+
+dir_id=-none
+[ -z $1 ] || dir_id=$1
+
+RUNDIR=`pwd`/tmpnor$dir_id
+mkdir $RUNDIR
+sudo mount -t tmpfs none $RUNDIR
+sudo chmod a+wr $RUNDIR
+cd $RUNDIR
+cp ../*sh .
+ln -s ../yaffs_test yaffs_test
+
+./init_fw_update_test_nor.sh
+./run_fw_update_test_nor.sh
+

6c7131b1d7c221125851a7c0bd183dd05a02d583
Charles Manning <cdhmanning@gmail.com>
2010-04-26 14:35:21 +1200

    yaffs: Rationalise plot_data.sh
    plot_data.sh now includes all the scripting required to do the plotting.
    No longer uses seperate scripts to gather data and drive gnu-plot.
    
    Signed-off-by: Charles Manning <cdhmanning@gmail.com>

--- linux-tests/plot_data.sh
+++ linux-tests/plot_data.sh
@@ -1,5 +1,60 @@
 #!/bin/sh
-# plot_data.sh runs the plot, updating it in realtime
+# Script that gathers data erased vs free data from /proc/yaffs_stats and simultaneously \
+# plots it using gnuplot.
 
-./drive_gnuplot.sh | gnuplot
+
+#Gather settings
+log_file=data
+gather_delay=1
+
+# Plot settings
+trunc_file=trunc_data
+plot_samples=1000
+plot_delay=2
+
+
+
+# Gathering task
+
+gather_data() {
+i=0;
+rm -f $log_file
+
+while true; do
+str=$(cat /proc/yaffs_debug)
+echo "$i, $str" 
+echo "$i, $str"  >> $log_file
+let i=$i+1
+sleep $gather_delay
+done
+}
+
+
+# Plotting task
+# Periodically creates a truncated version of the log file and
+# outputs commands into gnuplot, thus driving gnuplot
+
+drive_gnuplot(){
+sleep 5
+tail -$plot_samples $log_file > $trunc_file
+
+plot_str=" plot '$trunc_file' using 1:3 with linespoints title 'free', '' using 1:4 with linespoints title 'erased'"
+
+echo "set title 'yaffs free space and erased space'"
+
+echo $plot_str
+ 
+while true; do
+sleep $plot_delay
+tail -$plot_samples $log_file > $trunc_file
+echo replot
+done
+}
+
+
+
+echo "Start gathering task in background"
+gather_data &
+echo "Run plotting task"
+drive_gnuplot | gnuplot
 

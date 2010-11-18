af90cc3cb182162b1b3751eabbad81db084cedc2
Charles Manning <cdhmanning@gmail.com>
2010-11-18 13:47:47 +1300

    yaffs direst tests: Clean up error to string converter
    Compilation and comment clean ups.
    
    Signed-off-by: Charles Manning <cdhmanning@gmail.com>

--- direct/python/yaffs_error_converter.c
+++ direct/python/yaffs_error_converter.c
@@ -13,12 +13,12 @@
 
 #include "yaffs_error_converter.h"
 
-typedef struct error_codes_template {
+struct error_entry {
   int code;
   char * text;  
-}error_entry;
+};
 
-const error_entry error_list[] = {
+static const struct error_entry error_list[] = {
 	{ ENOMEM , "ENOMEM" },
 	{ EBUSY , "EBUSY"},
 	{ ENODEV , "ENODEV"},
@@ -43,9 +43,11 @@ const error_entry error_list[] = {
 
 const char * yaffs_error_to_str(int err)
 {
-	error_entry *e = error_list;
+	const struct error_entry *e = error_list;
+
 	if (err < 0) 
 		err = -err;
+
 	while(e->code && e->text){
 		if(err == e->code)
 			return e->text;
@@ -53,4 +55,3 @@ const char * yaffs_error_to_str(int err)
 	}
 	return "Unknown error code";
 }
-

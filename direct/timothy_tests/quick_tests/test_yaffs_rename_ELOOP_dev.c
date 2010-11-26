9a893d50905d6336770609b9de0d18ec2f3e280c
Timothy Manning <tfhmanning@gmail.com>
2010-11-26 15:12:04 +1300

    yaffs Added more tests to direct/timothy_tests/quick_tests Signed-off-by: Timothy Manning <tfhmanning@gmail.com>

---
+++ direct/timothy_tests/quick_tests/test_yaffs_rename_ELOOP_dev.c
@@ -0,0 +1,58 @@
+/*
+ * YAFFS: Yet another FFS. A NAND-flash specific file system.
+ *
+ * Copyright (C) 2002-2010 Aleph One Ltd.
+ *   for Toby Churchill Ltd and Brightstar Engineering
+ *
+ * Created by Timothy Manning <timothy@yaffs.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#include "test_yaffs_rename_ELOOP_dev.h"
+
+
+int test_yaffs_rename_ELOOP_dev(void)
+{
+	int output=0;
+	int error_code =0;
+
+	
+	if (set_up_ELOOP()<0){
+		print_message("failed to setup symlinks\n",2);
+		return -1;
+	}
+
+
+	output = yaffs_rename(ELOOP_PATH "/file" , RENAME_PATH);
+	if (output<0){ 
+		error_code=yaffs_get_error();
+		if (abs(error_code)==ELOOP){
+			return 1;
+		} else {
+			print_message("returned error does not match the the expected error\n",2);
+			return -1;
+		}
+	} else{
+		print_message("renamed a ELOOP (which is a bad thing)\n",2);
+		return -1;
+	}	
+}
+
+
+int test_yaffs_rename_ELOOP_dev_clean(void)
+{
+	int output = 0;
+	if (0 ==  yaffs_access(RENAME_PATH,0)) {
+		output = yaffs_rename(RENAME_PATH,FILE_PATH);
+		if (output < 0) {
+			print_message("failed to remove the directory\n",2);
+			return -1;
+		}
+	}
+	return 1;
+
+}
+
---
+++ direct/timothy_tests/quick_tests/test_yaffs_rename_ELOOP_dev.h
@@ -0,0 +1,25 @@
+/*
+ * YAFFS: Yet another Flash File System . A NAND-flash specific file system. 
+ *
+ * Copyright (C) 2002-2010 Aleph One Ltd.
+ *   for Toby Churchill Ltd and Brightstar Engineering
+ *
+ * Created by Timothy Manning <timothy@yaffs.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 2.1 as
+ * published by the Free Software Foundation.
+ *
+ * Note: Only YAFFS headers are LGPL, YAFFS C code is covered by GPL.
+ */
+
+#ifndef __test_yaffs_rename_ELOOP_dev_h__
+#define __test_yaffs_rename_ELOOP_dev_h__
+
+#include "lib.h"
+#include "yaffsfs.h"
+
+int test_yaffs_rename_ELOOP_dev(void);
+int test_yaffs_rename_ELOOP_dev_clean(void);
+
+#endif

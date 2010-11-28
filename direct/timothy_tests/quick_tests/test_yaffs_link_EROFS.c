c744d74c2f562bb505c3086585ae9beb359c85a3
Timothy Manning <tfhmanning@gmail.com>
2010-11-29 10:45:17 +1300

    yaffs Added morew tests to direct/timothy_tests/quick_tests Signed-off-by: Timothy Manning <tfhmanning@gmail.com>

---
+++ direct/timothy_tests/quick_tests/test_yaffs_link_EROFS.c
@@ -0,0 +1,62 @@
+/*
+ * YAFFS: Yet another FFS. A NAND-flash specific file system.
+ *
+ * Copyright (C) 2002-2010 Aleph One Ltd.
+ *   for Toby Churchill Ltd and Brightstar Engineering
+ *
+ * Created by Timothy Manning <timothy@yaffs.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#include "test_yaffs_link_ELOOP_dir.h"
+
+
+
+int test_yaffs_link_ELOOP_dir(void)
+{
+	int output=0;	
+	int error =0;
+
+	if (set_up_ELOOP()<0){
+		print_message("failed to setup symlinks\n",2);
+		return -1;
+	}
+
+	output = yaffs_link(ELOOP_PATH "/file",HARD_LINK_PATH);
+	if (output<0){
+		error=yaffs_get_error();
+		if (abs(error)==ELOOP){
+			return 1;
+		} else {
+			print_message("different error than expected\n",2);
+			return -1;
+		}
+	} else {
+		print_message("created a hard link to a non-existing-dir (which is a bad thing)\n",2);
+		return -1;	
+	}
+}
+
+
+int test_yaffs_link_ELOOP_dir_clean(void)
+{
+	int output=0;	
+	int error =0;
+	output= yaffs_unlink(HARD_LINK_PATH);
+	if (output<0){
+		error=yaffs_get_error();
+		if (abs(error)==ENOENT){
+			//if the file does not exist then the error should be ENOENT.
+			return 1;
+		} else {
+			print_message("different error than expected\n",2);
+			return -1;
+		}
+	} else {
+		return 1;	/* the file failed to open so there is no need to close it */
+	}
+}
+
